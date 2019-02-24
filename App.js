76f13212d8c761ca8665d8a0f75d24a4b71d0783
Alex Afeltra <afeltra.alex@gmail.com>
2019-02-24 15:07:26 -0500

    basic routing

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {createSwitchNavigator} from 'react-navigation'
 
-export default class App extends React.Component {
+class MainRouteComponent extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.mainContainer}> 
+        <Button title="go to movie screen" 
+                onPress={() => {
+                  this.props.navigation.navigate('MovieRoute')
+                }}/>
       </View>
     );
   }
 }
 
+class MovieRouteComponent extends React.Component {
+  render() {
+    return (
+      <View style={styles.movieContainer}> 
+        <Button title="go back" 
+                onPress={() => {
+                  this.props.navigation.navigate('MainRoute')
+                  }
+                }/>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createSwitchNavigator({
+  "MainRoute": MainRouteComponent,
+  "MovieRoute": MovieRouteComponent,
+})
+
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator />
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 25,
+    borderColor: 'teal',
+  },
+  movieContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 25,
+    borderColor: 'orange',
   },
 });
